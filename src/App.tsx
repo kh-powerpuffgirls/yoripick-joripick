3b81603147325c26974f79556a4c7ff35e479371
KHJ702 <k94070e@gmail.com>
2025-09-02 12:45:59 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import CommunityMain from './pages/community/CommunityMain' // 커뮤니티 메인 컴포넌트 불러오기
-import MyPost from './pages/community/mypost/MyPost'; // MyPost 컴포넌트 불러오기
-=======
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -13,7 +6,8 @@ import { AlertModal } from './components/AlertModal'
 import { ChatModal } from './components/Chatting/chatModal'
 import Mainpage from './pages/mainpage/Mainpage'
 import { CServiceMain } from './pages/CService/main'
->>>>>>> master
+import CommunityMain from './pages/community/CommunityMain' // 커뮤니티 메인 컴포넌트 불러오기
+import MyPost from './pages/community/mypost/MyPost'; // MyPost 컴포넌트 불러오기
 
 function App() {
   return (
@@ -22,15 +16,12 @@ function App() {
       <AlertModal />
       <ChatModal />
       <Routes>
-<<<<<<< HEAD
+        <Route path="/home" element={<Mainpage/>} />
+        <Route path="/cservice" element={<CServiceMain/>} />
         {/* 커뮤니티 메인 페이지 */}
         <Route path="/" element={<CommunityMain />} />
         {/* 내 게시물 페이지 */}
         <Route path="/mypost" element={<MyPost />} />
-=======
-        <Route path="/home" element={<Mainpage/>} />
-        <Route path="/cservice" element={<CServiceMain/>} />
->>>>>>> master
       </Routes>
       <Footer/>
     </>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-// src/store.ts
-
-import { configureStore } from "@reduxjs/toolkit";
-import boardsReducer from '../features/boardsSlice';
-
-export const store = configureStore({
-    reducer: {
-        // 리듀서 작성 하는 곳
-        boards: boardsReducer,
-=======
 import { configureStore } from "@reduxjs/toolkit"
 import alert from '../features/alertSlice'
 import chat from '../features/chatSlice'
+import boardsReducer from '../features/boardsSlice';
 
 export const store = configureStore({
     reducer:{
         alert,
-        chat
->>>>>>> master
+        chat,
+        boards: boardsReducer
     }
 });
 
