a99d3cfddb4575e6d5650db37fa36e526a29b0fa
DESKTOP-RGULR25\user1 <dkgus8551@gmail.com>
2025-09-18 17:12:15 +0900

    no message

---
+++ src/api/adminApi.ts
@@ -0,0 +1,9 @@
+export type PageInfo = {
+    listCount: number;
+    currentPage: number;
+    pageLimit: number;
+    itemLimit: number;
+    maxPage: number;
+    startPage: number;
+    endPage: number;
+};
