e567334c5dc8025000e23768c5393383e88e5875
DESKTOP-RGULR25\user1 <dkgus8551@gmail.com>
2025-09-24 14:24:48 +0900

    내 재료 모달 파일 생성

---
+++ src/components/IngModal/myIngNotification.tsx
@@ -0,0 +1,78 @@
+import { useSelector, useDispatch } from "react-redux";
+import style from "../Chatting/Notification.module.css";
+import type { RootState } from "../../store/store";
+import { removeNotification, startClosingAnimation } from "../../features/notiSlice";
+import type { Message } from "../../type/chatmodal";
+import { openChat } from "../../features/chatSlice";
+import React, { useEffect, useState } from "react";
+
+export const Notification = () => {
+  const dispatch = useDispatch();
+  const notifications = useSelector((state: RootState) => state.noti.list);
+  const ings = useSelector((state: RootState) => state.mying.ings);
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    setIsShown(true);
+  }, []);
+
+  const handleStartClose = (messageId: string, event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    dispatch(startClosingAnimation(messageId));
+  };
+
+  const handleClick = (message: Message) => {
+    // const room = rooms.find((r) => r.roomNo == message.roomNo);
+    // if (room) {
+    //   dispatch(openChat(room));
+    //   if (message.id) {
+    //     dispatch(removeNotification(message.id));
+    //   }
+    // }
+  };
+
+  return (
+    <div>
+      {notifications.map((message) => (
+        <div
+          key={message.id}
+          className={`${style.notiItem} ${isShown ? style.slideUp : ""} ${message.isClosing ? style.fadeOut : ""
+            }`}
+          onAnimationEnd={(e) => {
+            if (e.animationName === style.fadeOut) {
+              dispatch(removeNotification(message.id as string));
+            }
+          }}
+        >
+          <div className={style.notiHeader}>
+            <h3>새로운 메세지</h3>
+            {/* <span>{ings.find((r) => r.ingNo == message.roomNo)?.className}</span> */}
+          </div>
+          <div className={style.notiContent}>
+            <div className={style.profile}></div>
+            <div className={style.messageBody}>
+              <p>
+                <strong>{message.username}</strong>
+              </p>
+              <p className={style.messageText}>{message.content}</p>
+            </div>
+          </div>
+          <div className={style.notiActions}>
+            <button
+              className={`${style.notiBtn} ${style.cancelBtn}`}
+              onClick={(e) => handleStartClose(message.id as string, e)}
+            >
+              취소
+            </button>
+            <button
+              className={`${style.notiBtn} ${style.moveBtn}`}
+              onClick={() => handleClick(message)}
+            >
+              이동
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
