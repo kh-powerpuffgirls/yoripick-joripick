28f375c39f28e0682249222cfad07ced3709fc07
530hyelim <530hyelim@gmail.com>
2025-09-29 15:05:55 +0900

    no message

--- src/pages/community/Recipe/SikBti.tsx
+++ src/pages/community/Recipe/SikBti.tsx
@@ -7,22 +7,22 @@ interface SikBtiProps {
 }
 
 // ✨ 컴포넌트 이름은 대문자로 시작하는 것이 React의 규칙입니다. (sikBti -> SikBti)
-const SikBti: React.FC<SikBtiProps> = ({ sikBti,style }) => {
+const SikBti: React.FC<SikBtiProps> = ({ sikBti, style }) => {
   // 값에 따라 색상 지정
   const getColor = (bti: string) => {
     switch (bti) {
       case '육식 티라노': return '#FF7C7C';
       case '초식 트리케라톱스': return '#92CA02';
       case '바다의 연인': return '#2B90CE';
-      case '칼로리 스나이퍼': return '#0902CA';
-      case '열량 폭주기관차': return '#CA0202';
-      case '슴슴슴슴': return '#40E0D0';
+      case '원X스 브룩': return '#0902CA';
+      case '작지않은 빅맘': return '#CA0202';
+      case '한강라면': return '#40E0D0';
       case '도파민 중독자': return '#FD5CA8';
       case '잡식 햄스터': return '#FF6600';
       default: return 'gray'; // 기본값
     }
   };
-  
+
   return (
     <div
       style={{
